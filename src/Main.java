6dc3e393e91d784e9bf8f19a6b656c60574fb60e
dvlunev <dvlunev@ya.ru>
2022-11-08 11:56:32 +0300

    Мой первый коммит

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int varInt = 1000000000;
+        byte varByte = 100;
+        short varShort= 10000;
+        long varLong = 1000000000;
+        float varFloat = 3;
+        double varDouble = 3.111111111111111;
+        System.out.println("Значение переменной varInt с типом int равно " + varInt);
+        System.out.println("Значение переменной varByte с типом byte равно " + varByte);
+        System.out.println("Значение переменной varShort с типом short равно " + varShort);
+        System.out.println("Значение переменной varLong с типом long равно " + varLong);
+        System.out.println("Значение переменной varFloat с типом float равно " + varFloat);
+        System.out.println("Значение переменной varDouble с типом double равно " + varDouble);
+
+
+    }
+}
